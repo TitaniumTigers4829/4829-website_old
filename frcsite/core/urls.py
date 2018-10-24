7e50d39a89dfd2cf574e549934bf33f5183e71ce
DavidHan123 <43828888+DavidHan123@users.noreply.github.com>
2018-10-24 16:05:18 -0400

    Added new stuff

--- frcsite/core/urls.py
+++ frcsite/core/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-
+    path('calendar/', views.about, name='calendar'),
+    path('donations/', views.about, name='donations'),
+    path('sponsors/', views.about, name='sponsors'),
+    path('timeline/', views.about, name='calendar'),
 ]
