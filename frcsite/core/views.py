248c25d4453531d8983d87aa10f78905df78cc2b
DavidHan123 <43828888+DavidHan123@users.noreply.github.com>
2018-10-24 16:11:10 -0400

    Added new pages

--- frcsite/core/views.py
+++ frcsite/core/views.py
@@ -5,3 +5,11 @@ def home(request):
     return render(request, 'core/home.html')
 def about(request):
     return render(request, 'core/about.html')
+def calendar(request):
+    return render(request, 'core/calendar.html')
+def donations(request):
+    return render(request, 'core/donations.html')
+def sponsors(request):
+    return render(request, 'core/sponsors.html')
+def timeline(request):
+    return render(request, 'core/timeline.html')
