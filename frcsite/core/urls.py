bc420b06a5291f7e2c2567abd6e3308e11dab86e
wiedmann-trey <treywiedmann@gmail.com>
2018-10-24 16:53:08 -0400

    Fixed urls

--- frcsite/core/urls.py
+++ frcsite/core/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('calendar/', views.about, name='calendar'),
-    path('donations/', views.about, name='donations'),
-    path('sponsors/', views.about, name='sponsors'),
-    path('timeline/', views.about, name='timeline'),
-    path('media/', views.about, name='media'),
-    path('contact/', views.about, name='contact'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('donations/', views.donations, name='donations'),
+    path('sponsors/', views.sponsors, name='sponsors'),
+    path('timeline/', views.timeline, name='timeline'),
+    path('media/', views.media, name='media'),
+    path('contact/', views.contact, name='contact'),
 ]
